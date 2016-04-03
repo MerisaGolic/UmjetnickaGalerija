b21cdf5844453ea64dd20b3f4cfa19513ce1aac4
MerisaGolic <mgolic1@etf.unsa.ba>
2016-04-03 18:44:16 +0200

    Drugi projektni zadatak

--- Spirala2/objava.js
+++ Spirala2/objava.js
@@ -39,7 +39,8 @@ function izracunajVrijeme(v)
 			return "Objavljeno prije " + protekloMinuta + " minutu.";
 		else if(protekloMinuta%10 < 5 && protekloMinuta%10 > 1)
 			return "Objavljeno prije " + protekloMinuta + " minute.";
-		
+		else 
+			return "Objavljeno prije " + protekloMinuta + " minuta.";
 		//minutu: 1, 21, 31, 41, 51
 		//minute: 2, 3, 4, 22, 23, 24
 		//minuta: 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ,16, 17, 18, 19, 20
